d6b044cfbd48c669ad5e8246d30b8ceabc892846
Shahid afridi <shahidafridics@gmail.com>
2022-12-01 19:03:24 +0530

    first commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Register from "./Pages/Register.jsx"
+import Login from "./Pages/Login.jsx"
+import WritePost from "./Pages/WritePost"
+import SinglePost from "./Pages/SinglePost"
+import Home from "./Pages/Home"
+import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer"
+import "./style.scss"
+const Layout =()=>{
+  return (<>
+  <Navbar/>
+  <Outlet/>
+  <Footer/>
+  </>)
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/post/:id",
+        element: <SinglePost/>,
+      },
+      {
+        path: "/write",
+        element: <WritePost/>,
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/write",
+    element: <WritePost/>,
+  },
+  {
+    path: "/single",
+    element: <SinglePost/>,
+  },
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+
+  <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
+
+
+
+
 
 export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src="https://logos.textgiraffe.com/logos/logo-name/Shahid-designstyle-boots-m.png" alt="" />
+        <span>Made with ❤️ and <b>Shahid</b>.</span>
+    </footer>
+  )
+}
+
+export default Footer
